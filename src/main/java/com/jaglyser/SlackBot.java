f818393aaec6b5fb1b0110fc3aa45221443e1fe2
Jaglyser <joelwblomster@gmail.com>
2022-01-14 19:12:25 +0100

    first commit

---
+++ src/main/java/com/jaglyser/SlackBot.java
@@ -0,0 +1,21 @@
+package com.jaglyser;
+
+/**
+ * Hello world!
+ *
+ */
+import com.slack.api.bolt.App;
+import com.slack.api.bolt.jetty.SlackAppServer;
+
+public class SlackBot {
+    public static void main(String[] args) throws Exception {
+        App app = new App();
+
+        app.command("/hello", (req, ctx) -> {
+            return ctx.ack(":wave: Hello!");
+        });
+
+        SlackAppServer server = new SlackAppServer(app);
+        server.start();
+    }
+}
